e9448c94f8747eb36a8da5f0b506920ce68eeff4
Pie315 <devonkaelberer@gmail.com>
2023-01-17 15:54:13 -0500

    display completed

--- javascript.js
+++ javascript.js
@@ -1,3 +1,33 @@
+let container = document.querySelector(".container");
+
+function hover(element) {
+    element.classList.add("hovered");
+}
+
+function makeRow(rows) {
+    let row = document.createElement("div");
+    row.classList.add("row")
+
+    for (let i = 0; i < rows; i++) {
+        let element = document.createElement("div");
+        element.classList.add("pixel");
+        element.addEventListener("mouseover", () => hover(element))
+
+        row.appendChild(element);
+    }
+    return(row);
+}
+
 function makeGrid(dimension) {
-    
+    nuke();
+    for (let i = 0; i < dimension; i++) {
+        container.appendChild(makeRow(dimension));
+    }
+    console.log(container);
+}
+
+function nuke() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
 }
